f87f20dc9e0802b02a879f497945e2cc9ed2da4e
interpreterK <rhpidfyre@tuta.io>
2023-05-28 23:06:38 -0400

    db

--- src/javascript/toggleClassList.js
+++ src/javascript/toggleClassList.js
@@ -1,15 +1,11 @@
 const sticky_header = document.getElementById("intersection-obs")
 const bottom_section = document.getElementById("bottom-section")
 
-const newToggler = (element, type) => {
-	//lol
-	new IntersectionObserver(([e]) => e.target.classList.toggle(type, e.intersectionRatio < 1), {threshold: [1]}).observe(element)
-}
+//lol
+const newToggler = (element, type) => new IntersectionObserver(([e]) => e.target.classList.toggle(type, e.intersectionRatio < 1), {threshold: [1]}).observe(element)
 
 // Topbar becomes sticky when scrolling
 newToggler(sticky_header, "stuck")
-// Get this working
-newToggler(bottom_section, "undim")
 
 export {
 	newToggler
